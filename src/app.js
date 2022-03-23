a30664c2016fff6dc7c68690e32dec85cb862239
Rodion <info@socialdevelop.biz>
2022-03-24 01:48:09 +0300

    feat: add form and api method to get post data

--- src/app.js
+++ src/app.js
@@ -3,8 +3,22 @@ import express from 'express';
 const app = express();
 const port = process.env.EXPOSE_PORT || 80;
 
+app.use(express.urlencoded({
+  extended: true,
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/templates');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index.hbs', {
+    title: 'Тестовый заголовок',
+  });
+});
+
+app.post('/', (req, res) => {
+  const { username } = req.body;
+  console.log(`get the username is "${username}"`);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
